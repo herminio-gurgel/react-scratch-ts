ec779cfd4e22a1f0256b72b81c1ee68588db3c67
Hermínio Gurgel <herminio.gurgel@gmail.com>
2025-05-02 17:26:52 +0000

    style: apply formatting and remove unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -18,7 +17,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+          }}
+        >
           count is {count}
         </button>
         <p>
